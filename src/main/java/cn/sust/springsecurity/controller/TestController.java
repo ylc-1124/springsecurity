c2290e2cc96619df51bf7d557ae6fc0630a6629d
杨良晨 <853703920@qq.com>
2021-07-15 23:01:36 +0800

    快速入门案例

---
+++ src/main/java/cn/sust/springsecurity/controller/TestController.java
@@ -0,0 +1,15 @@
+package cn.sust.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello security";
+    }
+}
